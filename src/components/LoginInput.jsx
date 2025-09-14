92f954951c7c60dbfab5922f96f4bf9503fc889a
hasanmubarok9 <hasanmubarok32@gmail.com>
2025-09-14 14:10:22 +0700

    implement login input component

---
+++ src/components/LoginInput.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+function LoginInput({
+  login
+}) {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onEmailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onPasswordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    login({ emai, password });
+  };
+
+  return (
+      <form onSubmit={onSubmitHandler} className="input-login">
+        <label htmlFor="email">Email</label>
+        <input type="email" placeholder="email" value={email} id="email" onChange={onEmailChangeHandler} />
+        <label htmlFor="email">Password</label>
+        <input type="password" placeholder="password" value={password} id="password" onChange={onPasswordChangeHandler} />
+        <button type="submit">Login</button>
+      </form>
+  )
+}
+
+export default LoginInput;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LoginInput from '../components/LoginInput';
+import { login } from '../utils/network-data';
 
 function LoginPage() {
+  async function onLogin({ email, password }) {
+    const { error, data } = await login({ email, password });
+
+    if (!error) {
+      loginSuccess(data);
+    }
+  }
   return (
     <section className="login-page">
       <h2>Login to use app, please.</h2>
-      <div className="input-login">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="email">Password</label>
-        <input type="password" id="password" />
-        <button type="button">Login</button>
-      </div>
+      <LoginInput login={onLogin} />
       <p>Don't have an account? <Link to="/register">Register here</Link></p>
     </section>
   )
